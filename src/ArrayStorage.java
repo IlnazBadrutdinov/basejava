f24d3c7869fc75a541e0de1916c68040ebf9acfd
IlnazBadrutdinov <47192208+IlnazBadrutdinov@users.noreply.github.com>
2019-05-12 22:59:41 +0300

    update ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,7 +3,6 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-
     int size; // раззмер хранилища
 
     void clear() {
@@ -13,85 +12,64 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-
         // проходим по всему хранилищу
-        for (int k=0; k<storage.length;k++)
-        {
+        for (int k=0; k<storage.length;k++){
             //если не пустое значение
-            if (storage[k] == null)
-            {
+            if (storage[k] == null){
               //присваиваем ему значение, записываем элемент в хранилище
               storage[k] = r;
-
               //увеличиваем размер хранилища, т.к. добавили элемент
-              size = k +1;
+              size++;
               return;
             }
         }
     }
 
     Resume get(String uuid) {
-
-       for(int i= 0; i<this.size; i++){
-           Resume r = storage[i];
-
+       for(int i= 0; i<size; i++){
            //если соответствует элемент хранилища входному элементу, возвращаем его
-           if(r.uuid.equals(uuid)){
-               return r;
+           if(this.storage[i].uuid.equals(uuid)){
+               return this.storage[i];
            }
        }
         return null;
     }
 
     void delete(String uuid) {
-
-        int index = 99999;
+        //не могу присвоить 0, так как индексация в массиве начинается с 0. И в этом случае будет не всегда корректно выполняться последнее условие
+        int index = -1;
         int i=0;
 
         //пока есть элементы
-        while (i<this.size)
-        {
-            Resume r = this.storage[i];
+        while (i<this.size) {
             // прооверка на соответсвие найденному элементу
-            if (r.uuid.equals(uuid))
-            {
+            if (this.storage[i].uuid.equals(uuid)) {
                 //сохраняем найденный индекс
                 index = i;
+                // сдвигаем элементы хранилища, затирая элемент найденный по индексу
+                for (int j = index; j < size - 1; i++) {
+                    this.storage[j] = this.storage[j + 1];
+                }
+                this.size--;
                 break;
             }
+            i++;
         }
-
-        // сдвигаем элементы хранилища, затирая элемент найденный по индексу
-        for (i = index; i < this.size - 1; i++)
-        {
-             this.storage[i] = this.storage[i + 1];
-        }
-
-
-        this.size--;
-
-        if (index == 99999) { return; }
-
     }
-
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-
         Resume[] result = new Resume[this.size];
-
         // проходим по хранилищу и возвращаем элементы
         for (int i = 0; i < this.size; i++) {
             result[i] = this.storage[i];
         }
-
         return result;
 
     }
 
     int size() {
-
        return this.size;
     }
 }
